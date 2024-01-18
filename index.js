b0299990421a3c4b66da0c0766d5b5c55898bfe2
Sameena1911 <sameenaferoz8@gmail.com>
2024-01-18 13:45:30 +0530

    updated

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-//SELECTING sidenav
+//sidenav
 var sidenav = document.getElementById("sidenav")
 var menuicon = document.getElementById("menu")
-menuicon.addEventListener("click",function()
-{
-    sidenav.style.right=0
+menuicon.addEventListener("click", function () {
+  sidenav.style.right = 0
 })
 var closeicon = document.getElementById("close")
-closeicon.addEventListener("click", function () 
-{
-    sidenav.style.right="-50%"
+closeicon.addEventListener("click", function () {
+  sidenav.style.right = "-50%"
 })
 
 //slider
@@ -23,7 +21,147 @@ function carousel() {
     x[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
+  if (slideIndex > x.length) { slideIndex = 1 }
+  x[slideIndex - 1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+
+//search function
+
+var productContainer = document.getElementById("product-container");
+var search = document.getElementById("search");
+var productList = productContainer.querySelectorAll("div")
+search.addEventListener("keyup", function () {
+  var enteredValue = event.target.value.toUpperCase()
+  for (count = 0; count < productList.length; count = count + 1) {
+    var productName = productList[count].querySelector("h1").textContent
+    if (productName.toUpperCase().indexOf(enteredValue) < 0) {
+      productList[count].style.display = 'none'
+    }
+    else {
+      productList[count].style.display = 'block'
+    }
+  }
+
+})
+
+//favourites
+
+var btnvar1 = document.getElementById('btnh1');
+function Toggle1() {
+  if (btnvar1.style.color == "red") {
+    btnvar1.style.color = "grey"
+  }
+  else {
+    btnvar1.style.color = "red"
+  }
+}
+
+var btnvar2 = document.getElementById('btnh2');
+function Toggle2() {
+  if (btnvar2.style.color == "red") {
+    btnvar2.style.color = "grey"
+  }
+  else {
+    btnvar2.style.color = "red"
+  }
+}
+
+var btnvar3 = document.getElementById('btnh3');
+function Toggle3() {
+  if (btnvar3.style.color == "red") {
+    btnvar3.style.color = "grey"
+  }
+  else {
+    btnvar3.style.color = "red"
+  }
+}
+
+var btnvar4 = document.getElementById('btnh4');
+function Toggle4() {
+  if (btnvar4.style.color == "red") {
+    btnvar4.style.color = "grey"
+  }
+  else {
+    btnvar4.style.color = "red"
+  }
+}
+
+var btnvar5 = document.getElementById('btnh5');
+function Toggle5() {
+  if (btnvar5.style.color == "red") {
+    btnvar5.style.color = "grey"
+  }
+  else {
+    btnvar5.style.color = "red"
+  }
+}
+
+var btnvar6 = document.getElementById('btnh6');
+function Toggle6() {
+  if (btnvar6.style.color == "red") {
+    btnvar6.style.color = "grey"
+  }
+  else {
+    btnvar6.style.color = "red"
+  }
+}
+
+var btnvar7 = document.getElementById('btnh7');
+function Toggle7() {
+  if (btnvar7.style.color == "red") {
+    btnvar7.style.color = "grey"
+  }
+  else {
+    btnvar7.style.color = "red"
+  }
+}
+
+var btnvar8 = document.getElementById('btnh8');
+function Toggle8() {
+  if (btnvar8.style.color == "red") {
+    btnvar8.style.color = "grey"
+  }
+  else {
+    btnvar8.style.color = "red"
+  }
+}
+
+//navbar
+
+var feature = document.getElementById("new");
+feature.addEventListener("click", function () {
+  alert("This section is under process!")
+})
+
+var mens = document.getElementById("mens");
+mens.addEventListener("click", function () {
+  alert("This section is under process!")
+})
+
+var womens = document.getElementById("womens");
+womens.addEventListener("click", function () {
+  alert("This section is under process!")
+})
+
+var kids = document.getElementById("kids");
+kids.addEventListener("click", function () {
+  alert("This section is under process!")
+})
+
+var sale = document.getElementById("sale");
+sale.addEventListener("click", function () {
+  alert("This section is under process!")
+})
+
+var cart = document.getElementById("cart");
+cart.addEventListener("click", function () {
+  alert("This section is under development")
+})
+
+var user = document.getElementById("user");
+user.addEventListener("click", function () {
+  alert("This section is under development")
+})
+
